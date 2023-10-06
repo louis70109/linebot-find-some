797e85ebea53ebbaa71cab4469e7b7353ea061b0
Nijia Lin <louis70109@gmail.com>
2023-10-06 23:37:57 +0800

    ✨ init

---
+++ README.md
@@ -0,0 +1,89 @@
+# LINE Bot v3 with LangChain
+
+## 概述
+
+看到一個不知道是什麼的餐廳或食物時是不是覺得困擾？老師講的東西聽不懂？朋友在聊股票聽不懂？
+
+此專案結合 LangChain+OpenAI，幫助你快速把一句不確定的文字，透過 LINE Bot 介面提供給你答案
+
+## 環境變數
+
+你需要設置以下環境變數：
+
+- API_ENV: 應用程式的運行環境，可以是 'production' 或 'develop'，預設 develop。
+- LOG: 紀錄的等級，例如 'WARNING'、'INFO'、'DEBUG' 等。
+- LINE_CHANNEL_ACCESS_TOKEN: LINE Channel 的 Access Token。
+- LINE_CHANNEL_SECRET: LINE Channel 的 Secret。
+- OPENAI_API_KEY: OpenAI 的 API Key。
+- PORT: 預設 8080
+
+## 開發方法
+
+```
+git clone https://github.com/louis70109/linebot-find-some
+cd linebot-find-some/
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python main.py
+```
+
+透過 ngrok 建立暫時含有 https 的網址，並設定在 LINE Developer Console
+
+```
+ngrok http 8080
+```
+
+運用 `change_bot_url.sh` 來替換 LINE Bot webhook URL
+
+```
+sh change_bot_url.sh LINE_ACCESS_TOKEN https://YOUR_DOMAIN/webhooks/line
+```
+
+將 Domain 設定於 LINE Developer Console
+
+1. 前往 LINE Developer Console 網站：https://developers.line.biz/console/
+2. 點選右上角的「Log in」按鈕，登入您的 LINE 帳號。如果您還沒有 LINE 帳號，您需要先建立一個。
+3. 在網頁上方的選單中，選擇「Provider」，然後選擇「Create」。
+4. 在彈出的視窗中，輸入一個 Provider 名稱，然後點擊「Create」。
+5. 建立隻後，您將被導向該 Provider 的頁面。在這裡，選擇「Create Channel」。
+6. 您需要選擇您要創建的 Channel 類型，對於 bot，請選擇「Messaging API」。
+7. 填寫 Channel 的資訊，包括 Channel 名稱、Channel 說明、區域、大頭貼等等，然後點選「Create」。
+8. Channel 建立後，您將被導向 Channel 的設定頁面。在這裡，您可以看到您的 Channel ID、Channel Secret、以及 Channel Access Token，這些資訊將被用於設定 bot。
+9. 還有一個重要的設定是「Use webhooks」。如果您希望您的 bot 可以接收來自 LINE 的訊息，您需要打開這個設定，並輸入您的 webhook URL。您的 webhook URL 是一個可以接收 POST 請求的伺服器網址。
+10. 之後，您可以在 LINE app 中加入您剛剛建立的 bot 為好友，並開始測試。
+
+## Google Cloud Platform 佈署
+
+### 一鍵佈署
+
+[![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
+
+Clone 此專案
+
+```
+git clone https://github.com/louis70109/linebot-find-some
+cd linebot-find-some/
+```
+
+### gcloud 基礎設定
+
+- `gcloud init`：初始化 gcloud CLI，該指令會提示登錄 Google 帳戶，並選擇您要使用的 GCP 項目。
+- `gcloud config set project PROJECT_ID`：設定 GCP Project ID，以便 gcloud CLI 與該項目交互使用。
+- `gcloud auth login`：登錄 Google 帳戶。
+
+透過 [gcloud](https://cloud.google.com/sdk/docs/install?hl=zh-cn) 指令佈署
+
+```
+gcloud run deploy calendar-linebot-1 --source .
+```
+
+> 佈署參考: [【GCP】將 FastAPI 佈署上 Cloud Run](https://nijialin.com/2023/03/19/gcp-why-need-cloudrun-as-serverless/#5-%E4%BD%88%E7%BD%B2%E5%88%B0-Google-Cloud-Run)
+
+## 參與貢獻
+
+如果你有任何問題或建議，歡迎開 issue 或 pull request。
+
+## LICENSE
+
+請見 LICENSE 文件。
---
+++ main.py
@@ -0,0 +1,117 @@
+from stock_peformace import StockPercentageChangeTool, StockGetBestPerformingTool
+from stock_price import StockPriceTool
+from langchain.schema import HumanMessage
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
+import logging
+import os
+from wikipedia import WikiTool
+
+from youtube_restaurant import FindYoutubeVideoTool
+
+
+if os.getenv('API_ENV') != 'production':
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+import uvicorn
+from fastapi import FastAPI, Request
+from linebot.v3.webhook import WebhookParser
+from linebot.v3.messaging import (
+    AsyncApiClient,
+    AsyncMessagingApi,
+    Configuration,
+    ReplyMessageRequest,
+    PushMessageRequest,
+    TextMessage
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+logging.basicConfig(level=os.getenv('LOG', 'WARNING'))
+logger = logging.getLogger(__file__)
+
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+configuration = Configuration(
+    access_token=channel_access_token
+)
+
+app = FastAPI()
+async_api_client = AsyncApiClient(configuration)
+line_bot_api = AsyncMessagingApi(async_api_client)
+parser = WebhookParser(channel_secret)
+
+
+# Langchain (you must use 0613 model to use OpenAI functions.)
+model = ChatOpenAI(model="gpt-3.5-turbo-0613")
+tools = [
+    StockPriceTool(), StockPercentageChangeTool(),
+    StockGetBestPerformingTool(), FindYoutubeVideoTool(),
+    WikiTool()
+    ]
+open_ai_agent = initialize_agent(
+    tools,
+    model,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    verbose=False)
+
+
+@app.post("/webhooks/line")
+async def handle_callback(request: Request):
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = await request.body()
+    body = body.decode()
+
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    for event in events:
+        print(event)
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessageContent):
+            continue
+        # await line_bot_api.push_message(push_message_request=PushMessageRequest(
+        #     to=event.source.user_id,
+        #     messages=[TextMessage(text=event.message.text,
+        #                           quoteToken=event.message.quote_token)],
+        # ))
+
+        tool_result = open_ai_agent.run(event.message.text)
+
+        await line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=tool_result)]
+            )
+        )
+
+    return 'OK'
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', default=8080))
+    debug = True if os.environ.get(
+        'API_ENV', default='develop') == 'develop' else False
+    logging.info('Application will start...')
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=debug)
